7e81007c3987f7cd3a7ce7842cf218909d5d2598
saukrIppl <colinsippl@gmx.de>
2017-02-26 15:21:09 +0100

    Test
    fdsa

--- nltk-analyse/MainController.py
+++ nltk-analyse/MainController.py
@@ -30,13 +30,69 @@ def main():
 
     dylanIsOnlyAuthor = True
     allAuthors = False
-    intervalls = [(1960, 2019, dylanIsOnlyAuthor), (1960, 1969, dylanIsOnlyAuthor), (1970, 1979, dylanIsOnlyAuthor),
-                  (1980, 1989, dylanIsOnlyAuthor), (1990, 1999, dylanIsOnlyAuthor), (2000, 2009, dylanIsOnlyAuthor),
-                  (2010, 2019, dylanIsOnlyAuthor),
-                  (1960, 2019, allAuthors), (1960, 1969, allAuthors), (1970, 1979, allAuthors), (1980, 1989, allAuthors),
-                  (1990, 1999, allAuthors), (2000, 2009, allAuthors), (2010, 2019, allAuthors)
-                  ]
-    #intervalls = [(1960, 2019)]
+    intervalls = [
+        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Brown, D. (2014). Bob Dylan: American troubadour. Lanham, Md. [u.a.]: Rowman & Littlefield.
+
+        # Becoming Bob Dylan
+        (1960, 1964, dylanIsOnlyAuthor),
+        (1960, 1964, allAuthors),
+
+        # Electric Dylan
+        (1965, 1966, dylanIsOnlyAuthor),
+        (1965, 1966, allAuthors),
+
+        # Rural Glory
+        (1967, 1967, dylanIsOnlyAuthor),
+        (1967, 1967, allAuthors),
+
+        # "Take Me As I Am"
+        (1968, 1973, dylanIsOnlyAuthor),
+        (1968, 1973, allAuthors),
+
+        # Back in the Rain
+        (1974, 1978, dylanIsOnlyAuthor),
+        (1974, 1978, allAuthors),
+
+        # The Changing of the Guard
+        (1978, 1981, dylanIsOnlyAuthor),
+        (1978, 1981, allAuthors),
+
+        # Rock and Roll Dreams
+        (1983, 1990, dylanIsOnlyAuthor),
+        (1983, 1990, allAuthors),
+
+        # Good Enough for Now
+        (1989, 1997, dylanIsOnlyAuthor),
+        (1989, 1997, allAuthors),
+
+        # Bob Dylan Revisited
+        (2000, 2012, dylanIsOnlyAuthor),
+        (2000, 2012, allAuthors),
+
+        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Gesamtkorpus 1960 bis Gegenwart
+        (1960, 2019, dylanIsOnlyAuthor),
+        (1960, 2019, allAuthors),
+
+        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Dekaden
+        (1960, 1969, dylanIsOnlyAuthor), (1960, 1969, allAuthors),
+        (1970, 1979, dylanIsOnlyAuthor), (1970, 1979, allAuthors),
+        (1980, 1989, dylanIsOnlyAuthor), (1980, 1989, allAuthors),
+        (1990, 1999, dylanIsOnlyAuthor), (1990, 1999, allAuthors),
+        (2000, 2009, dylanIsOnlyAuthor), (2000, 2009, allAuthors),
+        (2010, 2019, dylanIsOnlyAuthor), (2010, 2019, allAuthors),
+
+        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Dylans Selbstfindung
+        (1964, 1965, dylanIsOnlyAuthor), (1964, 1965, allAuthors),
+
+        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Sonstiges
+        (1965, 1970, dylanIsOnlyAuthor), (1965, 1970, allAuthors),
+
+        ]
 
     #st = SnowballStemmer("english", ignore_stopwords=True)
     st = SnowballStemmer("english")
